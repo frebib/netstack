a805cfe910319e1d18fd3d2f4e8d58637bcaa8d9
Joe Groocock <frebib@gmail.com>
2017-08-15 16:30:05 +0100

    libnet/tcp: Move fmt_tcp_flags() impl to tcp.c

--- include/libnet/tcp/tcp.h
+++ include/libnet/tcp/tcp.h
@@ -66,22 +66,4 @@ struct tcp_hdr {
 
 int fmt_tcp_flags(struct tcp_hdr* hdr, char* buffer);
 
-int fmt_tcp_flags(struct tcp_hdr *hdr, char *buffer) {
-    if (hdr == NULL) {
-        return 1;
-    }
-
-    buffer[0] = (char) (hdr->fin ? 'F' : '.');
-    buffer[1] = (char) (hdr->syn ? 'S' : '.');
-    buffer[2] = (char) (hdr->rst ? 'R' : '.');
-    buffer[3] = (char) (hdr->psh ? 'P' : '.');
-    buffer[4] = (char) (hdr->ack ? 'A' : '.');
-    buffer[5] = (char) (hdr->urg ? 'U' : '.');
-    buffer[6] = (char) (hdr->ece ? 'E' : '.');
-    buffer[7] = (char) (hdr->cwr ? 'C' : '.');
-    buffer[8] = 0;
-
-    return 0;
-}
-
 #endif //NETD_TCP_H
