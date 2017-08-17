05cdd01cf300c05050a952f93d5423aca92dbd47
Joe Groocock <frebib@gmail.com>
2017-08-17 16:40:35 +0100

    libnet/tcp: Group flags in inner struct

--- include/libnet/tcp/tcp.h
+++ include/libnet/tcp/tcp.h
@@ -34,6 +34,7 @@ struct tcp_hdr {
                 dport;          /* Destination port */
     uint32_t    seqn,           /* Sequence number */
                 ackn;           /* Acknowledgement number */
+    struct {
 #if THE_HOST_IS_BIG_ENDIAN
     uint8_t     data_ofs:4,     /* Size of TCP header in 32-bit words */
                 rsvd:4,         /* Empty reserved space for ctrl flags */
@@ -59,6 +60,7 @@ struct tcp_hdr {
                 ece:1,
                 cwr:1;
 #endif
+    } flags;
     uint16_t    wind,           /* Size of the receive window */
                 csum,           /* Internet checksum */
                 urg_ptr;        /* Urgent data pointer */
--- lib/tcp/tcp.c
+++ lib/tcp/tcp.c
@@ -26,14 +26,14 @@ int fmt_tcp_flags(struct tcp_hdr *hdr, char *buffer) {
         return -1;
     }
 
-    buffer[0] = (char) (hdr->fin ? 'F' : '.');
-    buffer[1] = (char) (hdr->syn ? 'S' : '.');
-    buffer[2] = (char) (hdr->rst ? 'R' : '.');
-    buffer[3] = (char) (hdr->psh ? 'P' : '.');
-    buffer[4] = (char) (hdr->ack ? 'A' : '.');
-    buffer[5] = (char) (hdr->urg ? 'U' : '.');
-    buffer[6] = (char) (hdr->ece ? 'E' : '.');
-    buffer[7] = (char) (hdr->cwr ? 'C' : '.');
+    buffer[0] = (char) (hdr->flags.fin ? 'F' : '.');
+    buffer[1] = (char) (hdr->flags.syn ? 'S' : '.');
+    buffer[2] = (char) (hdr->flags.rst ? 'R' : '.');
+    buffer[3] = (char) (hdr->flags.psh ? 'P' : '.');
+    buffer[4] = (char) (hdr->flags.ack ? 'A' : '.');
+    buffer[5] = (char) (hdr->flags.urg ? 'U' : '.');
+    buffer[6] = (char) (hdr->flags.ece ? 'E' : '.');
+    buffer[7] = (char) (hdr->flags.cwr ? 'C' : '.');
     buffer[8] = 0;
 
     return 0;
