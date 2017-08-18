f9ee86311d856b8875d357320527cbe351e407c0
Joe Groocock <frebib@gmail.com>
2017-08-18 15:47:34 +0100

    libnet/eth: use macros and wrapped brackets correctly

--- include/libnet/eth/ether.h
+++ include/libnet/eth/ether.h
@@ -24,13 +24,12 @@ struct eth_hdr *eth_hdr(struct frame *frame);
 
 struct eth_hdr *recv_ether(struct frame *frame);
 
-/* String formatting functions */
 
 /* Formats a MAC address from an uint8_t[6] into a character buffer.
    WARNING: `buff` must be at least 18 characters in size as this macro
             does no bounds checking! */
 #define fmt_mac(a, buff) \
     sprintf(buff, "%02X:%02X:%02X:%02X:%02X:%02X", \
-        a[0], a[1], a[2], a[3], a[4], a[5])
+        (a)[0], (a)[1], (a)[2], (a)[3], (a)[4], (a)[5])
 
 #endif //NETD_ETHER_H
